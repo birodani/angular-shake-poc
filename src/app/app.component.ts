1a8ca8747ec8f935fba9c9b96bf5f85660423c72
birodani <birodani91@gmail.com>
2021-03-02 09:21:09 +0100

    add hg deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'shake-poc';
+
+  ngOnInit(): void {
+    let hasDeviceMotion = 'ondevicemotion' in window;
+    if(hasDeviceMotion) {
+      this.title = "Motion event available!";
+      document.addEventListener('ondevicemotion', x => alert("Shake!"));  
+      } else {
+        this.title = "Motion event unavailable!";
+      }
+  }
 }
